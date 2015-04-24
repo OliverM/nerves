5ab71d47a2db46f269b207a26c5de11bbf84ab24
Oliver Mooney <oliver.mooney@gmail.com>
2015-04-24 17:22:16 +0100

    Sketched out:  - a quick basic statechart with three states & actions between them only  - the event-action table that should result from it  - an action pump function that supplies a current state and an action name, and returns the updated state to be used in the next invocation

--- src/nerves/core.clj
+++ src/nerves/core.clj
@@ -5,23 +5,49 @@
 (defrecord State [])
 
 (def sample-state
-  {:history             :state                                         ;; :state | :deep
+  {:default             true                                            ;; if sibling states exist, this flag indiciates which is the default start state
+   :history             :state                                         ;; :state | :deep
    :concurrent-children true
    :children            []
    :name                "test-state"
-   :actions             [                                   ;; pairs of target state (matching :name) and action to perform
-                         ["destination-state" (fn [] nil)]
+   :actions             [;; triples of action name, target state (matching :name) and action to perform
+                         ["action-name" "destination-state" (fn [] nil)]
                          ]
+   })
 
-   }
-  )
+(def basic-statechart
+  [{:name    "StateA"
+    :default true
+    :actions [["frob"] ["StateB"] (fn [] "Frob action called")
+              ["blork"] ["StateC"] (fn [] "Blork action called")
+              ]}
+   {:name    "StateB"
+    :actions [["freb"] ["StateA"] (fn [] "Freb action called")
+              ["blerk"] ["StateC"] (fn [] "Blerk action called")
+              ]}
+   {:name    "StateC"
+    :actions [["frab"] ["StateB"] (fn [] "Frab action called")
+              ["blark"] ["StateA"] (fn [] "Blark action called")
+              ]}])
 
-(def sample-statechart
-  [{}
-   {}
-   {}])
+(defn statechart->eat
+  "Convert a statechart specification into a functioning event-action table"
+  [])
 
 (def sample-eat                                             ;; event action table
-  {
-   [[2 3 4] "test"] (fn [] "Test 1 fired")                  ;; requires a table-first invocation, e.g. (apply (sample-eat [[2 3 4] "test"]) nil)
-   })
+  {[[1] "frob"]  [(fn [] (println "Frob action called")) [2]]
+   [[1] "blork"] [(fn [] (println "Blork action called")) [3]]
+   [[2] "freb"]  [(fn [] (println "Freb action called")) [1]]
+   [[2] "blerk"] [(fn [] (println "Blerk action called")) [3]]
+   [[3] "frab"]  [(fn [] (println "Frab action called")) [2]]
+   [[3] "blark"] [(fn [] (println "Blark action called")) [1]]})
+
+(defn run-action
+  ""
+  [eat state action]
+  (let [[action-fn dest-state] (eat [state action])]
+    (action-fn)
+    dest-state))
+
+;; sample invocation
+(run-action sample-eat [3] "blark")                         ;; gives [1]
