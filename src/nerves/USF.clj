95dd4f431af4316d1087b63bbaa9b86dc5714082
Oliver Mooney <oliver.mooney@gmail.com>
2016-01-20 17:07:18 +0000

    Defined basic statechart sample in USF and captured basic (currently unpleasant) interaction sequence from the repl

--- src/nerves/USF.clj
+++ src/nerves/USF.clj
@@ -35,3 +35,31 @@
   (reify Guard
     (check [this metadata parameter] (guard-fn metadata parameter))))
 
+(defn basic-statechart-USF []
+  (let [statechart (Statechart. "basic-statechart" 2 false)
+        start-state (PseudoState. "begin" statechart PseudoState/pseudostate_start)
+        main-state (HierarchicalState. "main" statechart nil nil nil)
+        a-state (State. "A" main-state nil nil nil)
+        b-state (State. "B" main-state nil nil nil)
+        c-state (State. "C" main-state nil nil nil)]
+    (do
+      ; Transitions have a source state, destination state, event, guard and action. Implement as fn accepting a map of options?
+      (Transition. start-state a-state)                     ;; two arity form just specifies start & destination states
+      (Transition. a-state b-state (event "frob") nil nil)
+      (Transition. a-state c-state (event "blork") nil nil)
+      (Transition. b-state a-state (event "freb") nil nil)
+      (Transition. b-state c-state (event "blerk") nil nil)
+      (Transition. c-state a-state (event "frab") nil nil)
+      (Transition. c-state b-state (event "blark") nil nil)
+      statechart)
+    ))
+
+(comment
+  ;; repl session testing basic functionality
+  (def scdata (mydata "Test"))
+  (def basic-sc (basic-statechart-USF))
+  (.start basic-sc scdata)
+  (.dispatch basic-sc scdata (event "frob"))
+  ;; following returns true, indicating statechart is in state b after the "frob" event
+  (.isActive scdata (.getStateByName basic-sc "B"))
+  )
