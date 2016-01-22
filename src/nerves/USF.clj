1d4701dd5c52dd9822625856d6d1a736de7fa576
Oliver Mooney <oliver.mooney@gmail.com>
2016-01-22 17:21:46 +0000

    Refined USF transition creation

--- src/nerves/USF.clj
+++ src/nerves/USF.clj
@@ -23,6 +23,16 @@
          :leaf (State. name parent on-enter after-enter on-exit)
          :final (FinalState. name parent))))
 
+(defn transition
+  "Translates the USF Transition constructor into Clojure."
+  ([source destination] (Transition. source destination))
+  ([source destination event guard action] (Transition. source destination event guard action)))
+
+(defn connect-transitions
+  "Connects the supplied vector of transitions to the set of states. States must already have been instantiated."
+  [transitions]
+  (doall (map (partial apply transition) transitions)))
+
 (defn mydata
   "Try to hang some state on the statechart's Metadata. See http://kotka.de/blog/2010/03/proxy_gen-class_little_brother.html
   for reasons not to do this this way..."
@@ -60,17 +70,15 @@
         a-state (state :leaf "A" main-state)
         b-state (state :leaf "B" main-state)
         c-state (state :leaf "C" main-state)]
-    (do
-      ; Transitions have a source state, destination state, event, guard and action. Implement as fn accepting a map of options?
-      (Transition. start-state a-state)                     ;; two arity form just specifies start & destination states
-      (Transition. a-state b-state (event "frob") nil nil)
-      (Transition. a-state c-state (event "blork") nil nil)
-      (Transition. b-state a-state (event "freb") nil nil)
-      (Transition. b-state c-state (event "blerk") nil nil)
-      (Transition. c-state a-state (event "frab") nil nil)
-      (Transition. c-state b-state (event "blark") nil nil)
-      statechart)
-    ))
+    (connect-transitions
+          [[start-state a-state]
+           [a-state b-state (event "frob") nil nil]
+           [a-state c-state (event "blork") nil nil]
+           [b-state a-state (event "freb") nil nil]
+           [b-state c-state (event "blerk") nil nil]
+           [c-state a-state (event "frab") nil nil]
+           [c-state b-state (event "blark") nil nil]])
+    statechart))
 
 (comment
   ;; repl session testing basic functionality
